51d687b62eb53ace647926b856c6e61123c11e57
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-08-16 16:51:05 -0300

    Feita a alteração de CSS para que aplicação se torne responsiva ao utilizar telas menores como celulares.

--- src/pages/dashboard/home/home.jsx
+++ src/pages/dashboard/home/home.jsx
@@ -64,7 +64,7 @@ export function HomePage() {
                       {local.nome}
                     </Link>
                   </td>
-                  <td>{local.descricao}</td>
+                  <td>{local.descricao}`</td>
                 </tr>
               ))}
             </tbody>
