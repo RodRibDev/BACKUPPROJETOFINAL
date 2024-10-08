c99e45240493e14bf1c967726537b81a22795b2f
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-10-08 11:08:06 -0300

    Integração do login com backEnd e instação do Axios

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,47 +1,61 @@
 import { createContext, useContext, useState } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext({
     user: null,
     signIn: async () => {},
     signOut: () => {} 
-})
+});
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(() => {
-        const userLoggedStorage = localStorage.getItem('@nature365:user')
-
-        if(userLoggedStorage) {
-            return JSON.parse(userLoggedStorage)
+        const userLoggedStorage = localStorage.getItem('@nature365:user');
+        if (userLoggedStorage) {
+            return JSON.parse(userLoggedStorage);
         }
-
-        return null
-    })
+        return null;
+    });
 
     async function signIn(data) {
-        const response = await fetch(`http://localhost:3333/users?email=${data.email}`);
-        const userdata = await response.json()
-        
-        if(userdata.length > 0 && userdata[0].senha === data.senha){
-            setUser(userdata)
-            localStorage.setItem('@nature365:user', JSON.stringify(userdata))
-            return true
-        } else {
-            return false
+        try {
+            const response = await axios.post("http://localhost:3000/login", {
+                email: data.email,
+                password: data.senha
+            });
+            
+            if (response.status === 200) {
+                const token = response.data.Token;
+                const user = response.data.user;
 
+                localStorage.setItem('token', token);
+                localStorage.setItem('@nature365:user', JSON.stringify(user));
+
+                setUser(user);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error("Erro no login: ", error);
+            return false;
         }
-
     }
 
     function signOut() {
-        setUser(null)
-        localStorage.removeItem('@nature365:user')
+        
+        setUser(null);
+        localStorage.removeItem('@nature365:user');
+        localStorage.removeItem('token');
     }
 
-    return <AuthContext.Provider value={{ user, signIn, signOut }}>{children}</AuthContext.Provider>
+    return (
+        <AuthContext.Provider value={{ user, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export function useAuth() {
-    const contexto = useContext(AuthContext)
-
-    return contexto
+    const contexto = useContext(AuthContext);
+    return contexto;
 }
