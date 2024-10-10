53fc85ea4a58d13695836f0a31d892e939cb15d4
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-10-10 01:41:06 -0300

    Integração da página de local via axios.

--- src/pages/dashboard/local/local.jsx
+++ src/pages/dashboard/local/local.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import "./local.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import { Marcadores } from "../../../components/Marcadores";
 import "leaflet/dist/leaflet.css";
-
 
 const initialCoord = [-27.593754013143972, -48.565280761841805];
 
 export function Local() {
   const { id } = useParams();
-  const [locais, setLocais] = useState(null);
+  const [local, setLocal] = useState(null);
 
   useEffect(() => {
     async function fetchLocal() {
+      let token = localStorage.getItem('token');
       try {
-        const response = await fetch(`http://localhost:3333/locais/${id}`);
-        const data = await response.json();
-        setLocais(data);
+      
+        const response = await axios.get(`http://localhost:3000/local/${id}`, {
+          headers: {
+            'Authorization': `${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = response.data.listarLocal;
+        setLocal(data);
       } catch (error) {
         console.error("Erro ao buscar o local:", error);
       }
@@ -26,42 +33,38 @@ export function Local() {
     fetchLocal();
   }, [id]);
 
-  if (!locais) return <p>Carregando...</p>;
+  if (!local) return <p>Carregando...</p>;
 
   return (
     <div className="main-contentLocal">
       <div className="info-local">
-      <h2 className="fs-1" id="nomelocal">{locais.nome}</h2>
-      <p className="fw-bold">Descrição:</p>
-      <p id="descricaolocal">{locais.descricao}</p>
-      <p className="fw-bold">CEP:</p>
-      <p id="ceplocal">{locais.cep}</p>
-      <p className="fw-bold">Rua:</p>
-      <p id="rualocal">{locais.rua}</p>
-      <p className="fw-bold">Bairro:</p>
-      <p id="bairrolocal">{locais.bairro}</p>
-      <p className="fw-bold">Cidade:</p>
-      <p id="cidadelocal">{locais.cidade}</p>
-      <p className="fw-bold">UF:</p>
-      <p id="uflocal">{locais.uf}</p>
-      <p className="fw-bold">Latitude:</p>
-      <p id="latlocal">{locais.latitude}</p>
-      <p className="fw-bold">Longitude:</p>
-      <p id="longlocal">{locais.longitude}</p>
+        <h2 className="fs-1" id="nomelocal">{local.nome}</h2>
+        <p className="fw-bold">Descrição:</p>
+        <p id="descricaolocal">{local.descricao}</p>
+        <p className="fw-bold">CEP:</p>
+        <p id="ceplocal">{local.cep}</p>
+        <p className="fw-bold">Rua:</p>
+        <p id="rualocal">{local.rua}</p>
+        <p className="fw-bold">Bairro:</p>
+        <p id="bairrolocal">{local.bairro}</p>
+        <p className="fw-bold">Cidade:</p>
+        <p id="cidadelocal">{local.cidade}</p>
+        <p className="fw-bold">UF:</p>
+        <p id="uflocal">{local.uf}</p>
+        <p className="fw-bold">Latitude:</p>
+        <p id="latlocal">{local.latitude}</p>
+        <p className="fw-bold">Longitude:</p>
+        <p id="longlocal">{local.longitude}</p>
       </div>
 
       <div className="mapa-locais">
         <h2>Mapa</h2>
-        <p>Localização dos locais cadastrados</p>
+        <p>Localização do local cadastrado</p>
         <MapContainer center={initialCoord} zoom={13} className="map-container">
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marcadores locais={[locais]}></Marcadores>
+          <Marcadores locais={[local]}></Marcadores>
         </MapContainer>
       </div>
-
-
-      
     </div>
-    
   );
 }
